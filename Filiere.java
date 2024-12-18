1de01568911416acbc843c5d4cd32a96dbecdaa5
Fallou-Ndiaye78 <nfallou43@gmail.com>
2024-12-18 23:39:29 +0000

    git commit

--- Filiere.java
+++ Filiere.java
@@ -3,16 +3,7 @@ package GestionDesEnrolements;
 public class Filiere {
     private int id;
     private String nom;
-    private int nbPlaces;
+    private String description;
 
-    // Constructeur
-    public Filiere(int id, String nom, int nbPlaces) {
-        this.id = id;
-        this.nom = nom;
-        this.nbPlaces = nbPlaces;
-    }
-
-    public String getNbPlaces() {
-        return null;
-    }
+    // Constructeurs, getters, et setters
 }
